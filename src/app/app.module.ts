ad0d114f99655d68b13ac05b813902cce5c8b6f6
shaibachar@gmail.com <shaibachar@gmail.com>
2021-05-24 12:48:55 +0300

    tosave

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [SpeechRecognition,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private speechRecognition: SpeechRecognition) { }
+
+  start() {
+    // Check feature available
+    this.speechRecognition.isRecognitionAvailable()
+      .then((available: boolean) => console.log(available))
+
+    // Start the recognition process
+    this.speechRecognition.startListening()
+      .subscribe(
+        (matches: string[]) => console.log(matches),
+        (onerror) => console.log('error:', onerror)
+      )
+
+    // Stop the recognition process (iOS only)
+    this.speechRecognition.stopListening()
+
+    // Get the list of supported languages
+    this.speechRecognition.getSupportedLanguages()
+      .then(
+        (languages: string[]) => console.log(languages),
+        (error) => console.log(error)
+      )
+
+    // Check permission
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => console.log(hasPermission))
+
+    // Request permissions
+    this.speechRecognition.requestPermission()
+      .then(
+        () => console.log('Granted'),
+        () => console.log('Denied')
+      )
+  }
+}
